64a834c74c2efb2119324dad03ddba9d00de648c
Dark Bunny <neiloneil1337@gmail.com>
2017-07-25 10:14:08 -0400

    Update ChangeBG.sh

--- ChangeBG.sh
+++ ChangeBG.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-#psychø
-#Agent-2k40
+#
+# Code by: CØÐ'A®C; Agent-2k40
+# DarkBunny
 
 ChangeBG(){
-	echo "øøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøø";
-	echo "|øøøøøøøøøøøøøø    changeBG    øøøøøøøøøøøøøøøøøø|";
-	echo "øøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøøø";
-	echo "";
+	echo "=========================================="
+	echo "»»»»»»»»»»»»»»»» ChangeBG ««««««««««««««««"
+	echo "=========================================="
+	echo ""
+	echo "------------------------------------------"
+	echo " _ _"
+	echo "(˘©˘) Desenvolvido pensando no conforto"
+	echo ""
+	echo '"DarkBunny, estamos sempre por perto."'
+	echo "------------------------------------------"
+	echo ""
 }
 
-
 Change(){
+
 	export -f ChangeBG;
 
 	gnome-terminal -x bash -c '\
-	ChangeBG;\
-	read -p "[*] Intervalo entre papeis de parede (Seg): " TEMPO;\
-	while [[ "1" != "0" ]]; do
-		bash Setup.sh; sleep $TEMPO;
-	done; exec bash'
+				ChangeBG;\
+
+				echo " ";\
+
+				read -p "[!] Intervalo entre papeis de parede (Seg): " TEMPO; echo "";\
+
+				while [[ "1" != "0" ]]; do
+				bash Setup.sh; sleep $TEMPO;
+				done; exec bash'
 }
 
 Change;
